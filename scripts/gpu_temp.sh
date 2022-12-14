b80d91a10ad7f1b39cd58b45ab2f1f0fa92915ea
Bang Nguyen Anh <nguyenanhbang10102000@gmail.com>
2022-12-14 16:31:42 +0000

    fix for amd cpu

--- scripts/gpu_temp.sh
+++ scripts/gpu_temp.sh
@@ -22,7 +22,7 @@ print_gpu_temp() {
   fi
   tempC=$(echo "$loads" | sed -nr 's/.*\s([0-9]+)C.*/\1/p' | awk '{sum+=$1; n+=1} END {printf "%5.3f", sum/n}')
   if [ "$gpu_temp_unit" == "C" ]; then
-    echo "$tempC" | awk -v format="${gpu_temp_format}C" '{sum+=$1} END {printf format, sum}'
+    echo "$tempC" | awk -v format="${gpu_temp_format}°C" '{sum+=$1} END {printf format, sum}'
   else
     echo "$tempC" | awk -v format="${gpu_temp_format}F" '{sum+=$1} END {printf format, sum*9/5+32}'
   fi
